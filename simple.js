9ed398fe8e050633909b71115ce40db9ddde4c39
schnitzelbooz <turmandakh.ulziibayar@gmail.com>
2025-08-27 13:33:41 +0800

    interesting

--- simple.js
+++ simple.js
@@ -1,5 +1,7 @@
-const a = 1;
-const b = 2;
-const c = 3;
+const app = [1,2,3,1,2,3,4,12,3,4,34,5145,5,1454,2354,363]
+var sum = 0;
+for (let i = 0; i < app.length; i++) {
+    sum += app[i];
+}
 
-console.log(a, b, c);
+console.log(sum);
