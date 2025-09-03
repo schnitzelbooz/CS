d98ba0b5b618bf7b27d9589a5647b10d031c2f7d
schnitzelbooz <turmandakh.ulziibayar@gmail.com>
2025-09-03 11:41:23 +0800

    09.03

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-// --- Load Data ---
+
 function loadData() {
   return parseInt(localStorage.getItem("cafeteriaCount")) || 0;
 }
-
-// --- Save Data ---
 function saveData(count) {
   localStorage.setItem("cafeteriaCount", count);
 }
-
-// --- Load History ---
 function loadHistory() {
   let history = localStorage.getItem("cafeteriaHistory");
   return history ? JSON.parse(history) : [];
 }
-
-// --- Save History ---
 function saveHistory(history) {
   localStorage.setItem("cafeteriaHistory", JSON.stringify(history));
 }
-
-// --- Add to History ---
 function addHistory(action, newCount) {
   let history = loadHistory();
   let timestamp = new Date().toLocaleString(); // current time
   history.push({ action: action, count: newCount, time: timestamp });
   saveHistory(history);
 }
-
-// --- Increase Count ---
 function increase() {
   let count = loadData();
   count++;
@@ -35,8 +25,6 @@ function increase() {
   addHistory("Entered", count);
   showData();
 }
-
-// --- Decrease Count ---
 function decrease() {
   let count = loadData();
   if (count > 0) count--;
@@ -44,21 +32,14 @@ function decrease() {
   addHistory("Exited", count);
   showData();
 }
-
-// --- Reset Count & History ---
 function reset() {
   saveData(0);
   saveHistory([]);
   showData();
 }
-
-// --- Show Current Count & History ---
 function showData() {
-  // Show current congestion
   let count = loadData();
   document.getElementById("output").innerText = "Current count: " + count;
-
-  // Show history log
   let history = loadHistory();
   let historyList = document.getElementById("history");
   historyList.innerHTML = "";
